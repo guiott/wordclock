c3b136da6d34fafde7d3c385d2f52579e381f1d4
Guiott <guido@guiott.com>
2014-07-20 18:15:11 +0200

    Sync

--- WordClock.X/nbproject/Makefile-default.mk
+++ WordClock.X/nbproject/Makefile-default.mk
@@ -45,17 +45,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=../settings.c ../WordClock.c ../myi2c.c ../comm.c
+SOURCEFILES_QUOTED_IF_SPACED=../settings.c ../WordClock.c ../myi2c.c ../comm.c ../matrix.c ../timeProc.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/1472/settings.p1 ${OBJECTDIR}/_ext/1472/WordClock.p1 ${OBJECTDIR}/_ext/1472/myi2c.p1 ${OBJECTDIR}/_ext/1472/comm.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/1472/settings.p1.d ${OBJECTDIR}/_ext/1472/WordClock.p1.d ${OBJECTDIR}/_ext/1472/myi2c.p1.d ${OBJECTDIR}/_ext/1472/comm.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/1472/settings.p1 ${OBJECTDIR}/_ext/1472/WordClock.p1 ${OBJECTDIR}/_ext/1472/myi2c.p1 ${OBJECTDIR}/_ext/1472/comm.p1 ${OBJECTDIR}/_ext/1472/matrix.p1 ${OBJECTDIR}/_ext/1472/timeProc.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/1472/settings.p1.d ${OBJECTDIR}/_ext/1472/WordClock.p1.d ${OBJECTDIR}/_ext/1472/myi2c.p1.d ${OBJECTDIR}/_ext/1472/comm.p1.d ${OBJECTDIR}/_ext/1472/matrix.p1.d ${OBJECTDIR}/_ext/1472/timeProc.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/_ext/1472/settings.p1 ${OBJECTDIR}/_ext/1472/WordClock.p1 ${OBJECTDIR}/_ext/1472/myi2c.p1 ${OBJECTDIR}/_ext/1472/comm.p1
+OBJECTFILES=${OBJECTDIR}/_ext/1472/settings.p1 ${OBJECTDIR}/_ext/1472/WordClock.p1 ${OBJECTDIR}/_ext/1472/myi2c.p1 ${OBJECTDIR}/_ext/1472/comm.p1 ${OBJECTDIR}/_ext/1472/matrix.p1 ${OBJECTDIR}/_ext/1472/timeProc.p1
 
 # Source Files
-SOURCEFILES=../settings.c ../WordClock.c ../myi2c.c ../comm.c
+SOURCEFILES=../settings.c ../WordClock.c ../myi2c.c ../comm.c ../matrix.c ../timeProc.c
 
 
 CFLAGS=
@@ -110,6 +110,22 @@ ${OBJECTDIR}/_ext/1472/comm.p1: ../comm.c  nbproject/Makefile-${CND_CONF}.mk
 	@-${MV} ${OBJECTDIR}/_ext/1472/comm.d ${OBJECTDIR}/_ext/1472/comm.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/_ext/1472/comm.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
+${OBJECTDIR}/_ext/1472/matrix.p1: ../matrix.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR}/_ext/1472 
+	@${RM} ${OBJECTDIR}/_ext/1472/matrix.p1.d 
+	@${RM} ${OBJECTDIR}/_ext/1472/matrix.p1 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=none  --double=24 --float=24 --emi=wordwrite --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,+plib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/_ext/1472/matrix.p1  ../matrix.c 
+	@-${MV} ${OBJECTDIR}/_ext/1472/matrix.d ${OBJECTDIR}/_ext/1472/matrix.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/_ext/1472/matrix.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/_ext/1472/timeProc.p1: ../timeProc.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR}/_ext/1472 
+	@${RM} ${OBJECTDIR}/_ext/1472/timeProc.p1.d 
+	@${RM} ${OBJECTDIR}/_ext/1472/timeProc.p1 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=none  --double=24 --float=24 --emi=wordwrite --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,+plib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/_ext/1472/timeProc.p1  ../timeProc.c 
+	@-${MV} ${OBJECTDIR}/_ext/1472/timeProc.d ${OBJECTDIR}/_ext/1472/timeProc.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/_ext/1472/timeProc.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 else
 ${OBJECTDIR}/_ext/1472/settings.p1: ../settings.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/_ext/1472 
@@ -142,6 +158,22 @@ ${OBJECTDIR}/_ext/1472/comm.p1: ../comm.c  nbproject/Makefile-${CND_CONF}.mk
 	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --emi=wordwrite --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,+plib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/_ext/1472/comm.p1  ../comm.c 
 	@-${MV} ${OBJECTDIR}/_ext/1472/comm.d ${OBJECTDIR}/_ext/1472/comm.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/_ext/1472/comm.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/_ext/1472/matrix.p1: ../matrix.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR}/_ext/1472 
+	@${RM} ${OBJECTDIR}/_ext/1472/matrix.p1.d 
+	@${RM} ${OBJECTDIR}/_ext/1472/matrix.p1 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --emi=wordwrite --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,+plib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/_ext/1472/matrix.p1  ../matrix.c 
+	@-${MV} ${OBJECTDIR}/_ext/1472/matrix.d ${OBJECTDIR}/_ext/1472/matrix.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/_ext/1472/matrix.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/_ext/1472/timeProc.p1: ../timeProc.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR}/_ext/1472 
+	@${RM} ${OBJECTDIR}/_ext/1472/timeProc.p1.d 
+	@${RM} ${OBJECTDIR}/_ext/1472/timeProc.p1 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --emi=wordwrite --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,+plib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/_ext/1472/timeProc.p1  ../timeProc.c 
+	@-${MV} ${OBJECTDIR}/_ext/1472/timeProc.d ${OBJECTDIR}/_ext/1472/timeProc.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/_ext/1472/timeProc.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 endif
 
