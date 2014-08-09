749d0c657a6369d6ab290d0b6946edbf8f8382ff
Guiott <guido@guiott.com>
2014-08-09 20:11:43 +0200

    Matrix tested
    Matrix driving code tested on HW: http://youtu.be/Lgklh0TXdcQ

--- WordClock.X/nbproject/Makefile-variables.mk
+++ WordClock.X/nbproject/Makefile-variables.mk
@@ -11,3 +11,10 @@ CND_ARTIFACT_PATH_default=dist/default/production/WordClock.X.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
 CND_PACKAGE_NAME_default=wordclock.x.tar
 CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/wordclock.x.tar
+# SIM configuration
+CND_ARTIFACT_DIR_SIM=dist/SIM/production
+CND_ARTIFACT_NAME_SIM=WordClock.X.production.hex
+CND_ARTIFACT_PATH_SIM=dist/SIM/production/WordClock.X.production.hex
+CND_PACKAGE_DIR_SIM=${CND_DISTDIR}/SIM/package
+CND_PACKAGE_NAME_SIM=wordclock.x.tar
+CND_PACKAGE_PATH_SIM=${CND_DISTDIR}/SIM/package/wordclock.x.tar
